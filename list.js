7ceef0dc65679ab9eed4b799eef382d04ffeac15
David Lazic <lazic.ddavid@gmail.com>
2025-08-07 21:53:06 +0200

    added cover

--- list.js
+++ list.js
@@ -1,3 +1,15 @@
+let birthdays = [
+  { name: "Bertie Yates", age: 29, img: "person1.jpg" },
+  { name: "Hester Hogan", age: 32, img: "person2.jpg" },
+  { name: "Larry Little", age: 36, img: "person3.jpg" },
+  { name: "Sean Walsh", age: 34, img: "person4.jpg" },
+  { name: "Lola Gardner", age: 29, img: "person5.jpg" },
+];
+
+const listContainer = document.getElementById("list");
+const title = document.getElementById("title");
+const clearBtn = document.getElementById("clearBtn");
+
 function showBirthdays() {
   listContainer.innerHTML = "";
 
@@ -16,3 +28,11 @@ function showBirthdays() {
     listContainer.appendChild(personDiv);
   });
 }
+
+title.textContent = `${birthdays.length} birthdays today`;
+showBirthdays();
+
+clearBtn.addEventListener("click", function () {
+  birthdays.length = [];
+  showBirthdays();
+});
